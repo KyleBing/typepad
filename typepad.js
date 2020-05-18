b689f03e92073b8730037b70d24be7ab81788289
KyleBing <kylebing@163.com>
2020-05-18 11:32:34 +0800

    调整css目录，完成对错词显示的优化

--- typepad.js
+++ typepad.js
@@ -1,3 +1,19 @@
+// REG
+const REG = {
+  az: /^[a-zA-Z]$/,
+  number: /\d/,
+  function : /^(Control|Alt|Meta|Shift|Tab)$/,
+  ctrl : /^(Control|Alt|Meta|Shift)$/,
+  shift : /^Shift$/,
+  meta : /^Meta$/,
+  alt : /^Alt$/,
+  space : /^ $/,
+  backspace : /^Backspace$/,
+  delete : /^Delete$/,
+  semicolon: /;/,
+  quot: /'/,
+}
+
 class Count {
   az = 0;
   number = 0;
@@ -8,16 +24,12 @@ class Count {
   function = 0;
   space = 0;
   backspace = 0;
+  semicolon = 0;
+  quot = 0;
   init (){
-    this.az = 0;
-    this.number = 0;
-    this.ctrl = 0;
-    this.shift = 0;
-    this.meta = 0;
-    this.alt = 0;
-    this.function = 0;
-    this.backspace = 0;
-    this.space = 0;
+    for (let name in this) {
+      this[name] = 0;
+    }
   }
 }
 
@@ -50,6 +62,44 @@ class Engine {
     clearInterval(this.handleRefresh);
   }
 
+  compare(){
+    let typedWords = pad.value;
+    let arrayOrigin = currentWords.split('');
+    let arrayTyped = typedWords.split('');
+    let html = '';
+    let lastCharacterIsCorrect = false; // 上一个字符是正确的
+    let wordsCorrect = '';
+    let wordsWrong = '';
+    arrayTyped.forEach( (current, index) => {
+      let origin = arrayOrigin[index];
+      let currentCharacterIsCorrect = current === origin;
+      if (currentCharacterIsCorrect){
+        wordsCorrect = wordsCorrect.concat(origin);
+      } else {
+        wordsWrong = wordsWrong.concat(origin);
+      }
+
+      if (wordsCorrect && !lastCharacterIsCorrect && index){
+        html = html.concat(`<span class="wrong">${wordsWrong}</span>`);
+        wordsWrong = '';
+      } else if (wordsWrong && lastCharacterIsCorrect && index) {
+        html = html.concat(`<span class="correct">${wordsCorrect}</span>`);
+        wordsCorrect = '';
+      }
+      if ((index + 1) === typedWords.length) {
+        if (wordsCorrect) {
+          html = html.concat(`<span class="correct">${wordsCorrect}</span>`);
+        } else {
+          html = html.concat(`<span class="wrong">${wordsWrong}</span>`);
+        }
+      }
+      lastCharacterIsCorrect = current === origin;
+    })
+    let untypedString = currentWords.substring(arrayTyped.length)
+    html = html.concat(untypedString)
+    template.innerHTML = html;
+  }
+
   countSpeed(){
 
   }
@@ -71,6 +121,7 @@ class Engine {
     this.stopRefresh()
   }
   resume(){
+    this.timeStart = (new Date()).getTime() - this.duration;
     this.isPaused = false;
     this.startRefresh();
 
@@ -89,20 +140,23 @@ class Engine {
   }
 }
 
+
+const Before500 = '的一是了不在有个人这上中大为来我到出要以时和地们得可下对生也子就过能他会多发说而于自之用年行家方后作成开面事好小心前所道法如进着同经分定都然与本还其当起动已两点从问里主实天高去现长此三将无国全文理明日些看只公等十意正外想间把情者没重相那向知因样学应又手但信关使种见力名二处门并口么先位头回话很再由身入内第平被给次别几月真立新通少机打水果最部何安接报声才体今合性西你放表目加常做己老四件解路更走比总金管光工结提任东原便美及教难世至气神山数利书代直色场变记张必受交非服化求风度太万各算边王什快许连五活思该步海指物则女或完马强言条特命感清带认保望转传儿制干计民白住字它义车像反象题却流且即深近形取往系量论告息让决未花收满每华业南觉电空眼听远师元请容她军士百办语期北林识半夫客战院城候单音台死视领失司亲始极双令改功程爱德复切随李员离轻观青足落叫根怎持精送众影八首包准兴红达早尽故房引火站似找备调断设格消拉照布友整术石展紧据终周式举飞片虽易运笑云建谈界务写钱商乐推注越千微若约英集示呢待坐议乎留称品志黑存六造低江念产刻节尔吃势依图共曾响底装具喜严九况跟罗须显热病证刚治绝群市阳确究久除闻答段官政类黄武七支费父统';
+
 // Articles
 const ARTICLE = {
-  before500: '的一是了不在有个人这上中大为来我到出要以时和地们得可下对生也子就过能他会多发说而于自之用年行家方后作成开面事好小心前所道法如进着同经分定都然与本还其当起动已两点从问里主实天高去现长此三将无国全文理明日些看只公等十意正外想间把情者没重相那向知因样学应又手但信关使种见力名二处门并口么先位头回话很再由身入内第平被给次别几月真立新通少机打水果最部何安接报声才体今合性西你放表目加常做己老四件解路更走比总金管光工结提任东原便美及教难世至气神山数利书代直色场变记张必受交非服化求风度太万各算边王什快许连五活思该步海指物则女或完马强言条特命感清带认保望转传儿制干计民白住字它义车像反象题却流且即深近形取往系量论告息让决未花收满每华业南觉电空眼听远师元请容她军士百办语期北林识半夫客战院城候单音台死视领失司亲始极双令改功程爱德复切随李员离轻观青足落叫根怎持精送众影八首包准兴红达早尽故房引火站似找备调断设格消拉照布友整术石展紧据终周式举飞片虽易运笑云建谈界务写钱商乐推注越千微若约英集示呢待坐议乎留称品志黑存六造低江念产刻节尔吃势依图共曾响底装具喜严九况跟罗须显热病证刚治绝群市阳确究久除闻答段官政类黄武七支费父统',
+  common15: shuffle(Before500.split('')).slice(0,15).join('')
 }
 
-const comparison = $('.comparison p');
+const template = $('.template p');
 const pad = $('#pad');
 let count = new Count();
 let engine = new Engine();
-let currentWords = ARTICLE.before500;
+let currentWords = ARTICLE.common15;
 
 window.onload = () => {
   // init
-  comparison.innerText = currentWords;
+  template.innerText = currentWords;
   updateInfo();
 
   pad.onblur = () => {
@@ -119,7 +173,6 @@ window.onload = () => {
 
   // key pressed
   pad.onkeydown = (e) => {
-    console.log(e)
     if (e.key === 'Tab' ||
       ((e.metaKey||e.ctrlKey) && e.key === 'r')||
       ((e.metaKey||e.ctrlKey) && e.key === 's'))
@@ -134,27 +187,15 @@ window.onload = () => {
   pad.onkeyup = (e) => {
     e.preventDefault();
     countKeys(e);
+    engine.compare();
   }
-
 }
 
 
 
 
 
-// REG
-const REG = {
-  az: /^[a-zA-Z]$/,
-  number: /\d/,
-  function : /^(Control|Alt|Meta|Shift|Tab)$/,
-  ctrl : /^(Control|Alt|Meta|Shift)$/,
-  shift : /^Shift$/,
-  meta : /^Meta$/,
-  alt : /^Alt$/,
-  space : /^ $/,
-  backspace : /^Backspace$/,
-  delete : /^Delete$/,
-}
+
 
 // Count Key
 function countKeys(e) {
@@ -184,7 +225,7 @@ function updateInfo() {
 function shuffleWords() {
   let array = currentWords.split('');
   currentWords = shuffle(array).join('');
-  comparison.innerText = currentWords;
+  template.innerText = currentWords;
   engine.reset();
   updateInfo();
 }
