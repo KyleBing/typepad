c0a808782fb98c406e598f562dbf342bd5da46ce
KyleBing <kylebing@163.com>
2020-12-08 16:13:05 +0800

    适配暗黑模式

--- js/class/Database.js
+++ js/class/Database.js
@@ -110,7 +110,7 @@ define(['Utility', 'ArticleType'], function (Utility, ArticleType) {
               <td>${cursor.value.wordCount}</td>
               <td class="text-center ${textClass}"">${articleType}</td>
               <td>${cursor.value.articleName ? cursor.value.articleName : ''}</td>
-              <td class="hidden-sm">${Utility.dateFormatter(new Date(cursor.value.timeStart))}</td>
+              <td class="hidden-sm time">${Utility.dateFormatter(new Date(cursor.value.timeStart))}</td>
               <td class="time">${Utility.formatTimeLeft(cursor.value.duration)}</td>
               <td><button class="btn btn-danger btn-sm" onclick="engine.delete(${cursor.key}, this)" type="button">删除</button></td>
             </tr>`;
