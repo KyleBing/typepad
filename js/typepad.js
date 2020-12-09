a4932b5143f0d7035c094d17eb28e8cfb7f8c37f
KyleBing <kylebing@163.com>
2020-12-09 11:53:03 +0800

    v2.32

--- js/typepad.js
+++ js/typepad.js
@@ -73,7 +73,7 @@ require(['ArticleType', 'Article', 'Engine', 'Editor'],
       // Service Worker
       if ('serviceWorker' in navigator){
          navigator.serviceWorker
-            .register('/tools/typepad/typepad-sw.js?v=2.31')
+            .register('/tools/typepad/typepad-sw.js?v=2.32')
             .then(()=>{
                console.log('Server Worker has registered');
             })
