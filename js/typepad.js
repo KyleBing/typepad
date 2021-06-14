45d1ef2619a8e825d46f629f79b7c949833f1cd5
KyleBing <kylebing@163.com>
2021-06-14 16:33:15 +0800

    微调移动端展示

--- js/typepad.js
+++ js/typepad.js
@@ -73,7 +73,7 @@ require(['ArticleType', 'Article', 'Engine', 'Editor'],
       // Service Worker
       if ('serviceWorker' in navigator){
          navigator.serviceWorker
-            .register('/tools/typepad/typepad-sw.js?v=2.40')
+            .register('/tools/typepad/typepad-sw.js?v=2.41')
             .then(()=>{
                console.log('Server Worker has registered');
             })
