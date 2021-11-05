bcd2cb3b549967b83de2376b64be3278fd17d195
KyleBing <kylebing@163.com>
2021-11-05 22:58:13 +0800

    英文字体改为默认，不再使用 glivj

--- js/typepad.js
+++ js/typepad.js
@@ -73,7 +73,7 @@ require(['ArticleType', 'Article', 'Engine', 'Editor'],
       // Service Worker
       if ('serviceWorker' in navigator){
          navigator.serviceWorker
-            .register('/tools/typepad/typepad-sw.js?v=2.53')
+            .register('/tools/typepad/typepad-sw.js?v=2.54')
             .then(()=>{
                console.log('Server Worker has registered');
             })
